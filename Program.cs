32c4e44f19fda3e4ec9b4586d65029f00902cd34
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:23:49 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 1
+
+Console.WriteLine("Введите длины сторон прямоуголнька (целые числа):");
+string number1, number2;
+number1 = Console.ReadLine();
+int a = Convert.ToInt32(number1);
+number2 = Console.ReadLine();
+int b = Convert.ToInt32(number2);
+Console.WriteLine("Площадь прямоугольника = " + a * b);
